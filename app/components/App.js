1fec015dd518c82e227b6abd1c77866dc12d1d9e
Dmitry Zeldin <dmitry@zeldin.pro>
2018-01-03 22:10:32 +0100

    Finished bootstrap import. Initial page structure

--- app/components/App.js
+++ app/components/App.js
@@ -3,7 +3,15 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-        <h1>Hello World</h1>
+      <div className="container">
+        <header className="header clearfix">
+          <h3 className="text-muted">Currency converter</h3>
+        </header>
+
+        <main role="main">
+          <h1>Hello World</h1>
+        </main>
+      </div>
     )
   }
 }
--- app/index.js
+++ app/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import App from './components/app';
 import { render } from 'react-dom';
+
+window.$ = window.jQuery = require('jquery');
+window.Popper = require('popper.js');
 import 'bootstrap';
+import './styles/index.scss';
 
 render(
   <App/>,
