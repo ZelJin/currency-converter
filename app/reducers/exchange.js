a9c2d8b6c824e4a488793d6aa2fc066254fe79e1
Dmitry Zeldin <dmitry@zeldin.pro>
2018-01-07 01:00:43 +0100

    Implemented first iteration of rate recalculation

--- app/reducers/exchange.js
+++ app/reducers/exchange.js
@@ -17,7 +17,21 @@ const initialState = {
 export default function exchange(state = initialState, action) {
   switch(action.type) {
     case CHANGE_VALUE:
-      return state
+      var newState = Object.assign({}, state);
+      newState[action.name] = {
+        currency: action.currency,
+        value: action.value
+      }
+      if (isNaN(parseFloat(action.value))) {
+        return newState
+      }
+      var rate = newState.exchangeRates[newState.base.currency][newState.quote.currency];
+      if (action.name == 'base') {
+        newState.quote.value = parseFloat(newState.base.value) * rate;
+      } else {
+        newState.base.value = parseFloat(newState.quote.value) / rate;
+      }
+      return newState
     default:
       return state
   }
